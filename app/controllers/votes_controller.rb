458baa2e12be01397adb35383e31a055d221009a
My Name <myemail@example.com>
2014-02-28 17:19:01 -0500

    fixed votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,11 @@
 class VotesController < ApplicationController
 
   def create_upvote
-    binding.pry
-    @vote = Vote.new(params[:story])
+    @story = Story.find(params[:story])
+    @vote = Vote.new
     @vote.user = current_user
     @vote.choice = 1
+    @vote.story = @story
     @vote.save
     redirect_to root_path 
   end
@@ -16,12 +17,6 @@ class VotesController < ApplicationController
     @vote.save
 
     redirect_to root_path 
-  end
-
-  private
-
-  def vote_params
-    params.permit(:story)
   end
 
 end
