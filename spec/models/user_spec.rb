7607c28beac6c9d46cc469f05be5a5f735baae89
kdoyne <kdoyne930@gmail.com>
2014-02-28 12:37:28 -0500

    passing user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe User do
+  subject { FactoryGirl.create(:user) }
+  
+  it { should validate_presence_of(:email)}
+  it { should validate_presence_of(:username)}
+  it { should validate_uniqueness_of(:email)}
+  it { should validate_uniqueness_of(:username)}
+
+end
